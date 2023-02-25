92f7f6dd693d4e73a859047485caf989c1262dd1
yoga212121 <yogame733@gmail.com>
2023-02-25 18:36:30 +0530

    ML added

--- ML/EXIF.py
+++ ML/EXIF.py
@@ -2,7 +2,7 @@ from exif import Image
 from geopy.geocoders import Nominatim
 
 #image path must be in same folder
-img_path = "ML/food.jpg"       
+img_path = "Album-Maker/ML/food.jpg"    
 
 def decimal_coords(coords, ref):
     decimal_degrees = coords[0]+coords[1]/60+coords[2]/3600
@@ -32,28 +32,3 @@ def image_coordinates(img_path):
 
 image_coordinates(img_path)
 
-
-# from exif import Image
-# img_path="ML/IMG_20230223_213942.jpg"
-
-# def decimal_coords(coords,ref):
-#     decimal_degrees=coords[0]+coords[1]/60+coords[2]/3600
-#     if ref=="S" or ref=="W":
-#         decimal_degrees*=-1
-#     return decimal_degrees
-
-# def image_coordinates(img_path):
-#     with open(img_path,'rb') as src:
-#         img=Image(src)
-#     if img.has_exif:
-#         try:
-#             img.gps_longitude
-#             coords=(decimal_coords(img.gps_latitude,img.gps_latitude_ref),decimal_coords(img.gps_longitude,img.gps_longitude_ref))
-#         except AttributeError:
-#             print('No Coordinates')
-#     else:
-#         print('The Image has no EXIF Info')
-#     print(f"Image {src.name},OS Version:{img.get('software','Not Known')}---------")
-#     print(f"Was taken: {img.datetime_original},has coordinates:{coords}")
-
-# image_coordinates(img_path)
